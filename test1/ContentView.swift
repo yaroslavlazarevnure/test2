e67887afa28c4ca6f5e0dff4e15f4b31b3a926e2
yaroslavlazarevnure <38326468+yaroslavlazarevnure@users.noreply.github.com>
2024-09-05 19:36:46 +0200

    four

--- test1/ContentView.swift
+++ test1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("kmkkoo, world!")
+            Text("lklkk, world!")
         }
         .padding()
     }

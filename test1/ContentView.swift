12bf942706d435438e0d249d91faf5b2556bf5e9
yaroslavlazarevnure <38326468+yaroslavlazarevnure@users.noreply.github.com>
2024-09-05 19:13:22 +0200

    Initial Commit

---
+++ test1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test1
+//
+//  Created by Ярослав on 05.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
